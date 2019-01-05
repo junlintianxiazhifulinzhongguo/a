18a4400ad28eca7e4859113760c267cf37e03d77
root <root@localhost.localdomain>
2019-01-05 09:00:53 -0500

    ddds2dd

--- src/api/login.js
+++ src/api/login.js
@@ -16,11 +16,9 @@ export function loginByAlipay(){
   let url = request({
       url: '/api/auhRedirect',
       method: 'post'
-    }).then(response=>{
-      return response.data.auth_url
     })
   console.log(url)
-  return url    
+  //return url    
 }
 
 export function logout() {
