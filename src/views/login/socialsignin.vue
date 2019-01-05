a917491fed9e9afd313478bb5f0342f6581dfc54
root <root@localhost.localdomain>
2019-01-05 02:48:14 -0500

    2wd

--- src/views/login/socialsignin.vue
+++ src/views/login/socialsignin.vue
@@ -35,10 +35,11 @@ export default {
       // openWindow(url, thirdpart, 540, 540)
     },
     alipayHandleClick(thirdpart) {
-      alert('ok')
-      loginByAlipay().then(response => {
-        openWindow('https://www.baidu.com', thirdpart, 540, 540)   
-      })
+      // alert('ok')
+      openWindow('https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id=2018123062714467&scope=auth_user&redirect_uri=http://www.junlintianxiazhifulinzhongguo.top/api/auhRedirect', thirdpart, 540, 540)
+      // loginByAlipay().then(response => {
+      //   openWindow('https://www.baidu.com', thirdpart, 540, 540)   
+      // })
       // this.$store.commit('SET_AUTH_TYPE', thirdpart)
       // const client_id = 'xxxxx'
       // const redirect_uri = encodeURIComponent('xxx/redirect?redirect=' + window.location.origin + '/auth-redirect')
