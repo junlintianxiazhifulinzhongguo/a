0c8812c5a35d9777f5fc1cbe77499a34c9d634fc
root <root@localhost.localdomain>
2019-01-05 06:13:21 -0500

    dds

--- src/views/login/socialsignin.vue
+++ src/views/login/socialsignin.vue
@@ -37,10 +37,9 @@ export default {
     alipayHandleClick(thirdpart) {
       // alert('ok')
       // openWindow("https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id=2018123062714467&scope=auth_user&redirect_uri=http://www.junlintianxiazhifulinzhongguo.top/api/auhRedirect", thirdpart, 540, 540)
-      let a
-      loginByAlipay().then(function (data){
-         openWindow(data.auth_url, thirdpart, 540, 540)  
-      }) 
+      let a=await loginByAlipay()
+      console.log(a)
+      //openWindow(data.auth_url, thirdpart, 540, 540)  
       // this.$store.commit('SET_AUTH_TYPE', thirdpart)
       // const client_id = 'xxxxx'
       // const redirect_uri = encodeURIComponent('xxx/redirect?redirect=' + window.location.origin + '/auth-redirect')
