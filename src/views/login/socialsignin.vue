0fd11186e31e95b819b5e74ac01caf14507d735e
root <root@localhost.localdomain>
2019-01-05 08:28:23 -0500

    ddds

--- src/views/login/socialsignin.vue
+++ src/views/login/socialsignin.vue
@@ -34,10 +34,10 @@ export default {
       // const url = 'https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirect_uri
       // openWindow(url, thirdpart, 540, 540)
     },
-    alipayHandleClick(thirdpart) {
+    async alipayHandleClick(thirdpart) {
       // alert('ok')
       // openWindow("https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id=2018123062714467&scope=auth_user&redirect_uri=http://www.junlintianxiazhifulinzhongguo.top/api/auhRedirect", thirdpart, 540, 540)
-      let a = await loginByAlipay()
+      let a =await loginByAlipay()
       console.log(a)
       //openWindow(data.auth_url, thirdpart, 540, 540)  
       // this.$store.commit('SET_AUTH_TYPE', thirdpart)
